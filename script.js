ab89bddadc9538a7e5c4e0e4ee88b904b20abeb4
Chinmay1635 <chinmaykulkarni165@gmail.com>
2024-04-14 08:42:21 +0530

    Created HTML file and styled all elements

---
+++ script.js
@@ -0,0 +1,10 @@
+let btns = document.querySelectorAll(".opt");
+let ply = document.getElementById(".player");
+let comp = document.getElementById(".comp");
+let result = document.getElementById(".result");
+
+btns.forEach((btn) => {
+    btn.addEventListener("click",() => {
+        
+    })
+})
